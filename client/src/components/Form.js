07bfed04b181d503b5a4068b716c23b2937f3117
mealthebear <mealthebear@gmail.com>
2021-03-01 02:41:39 -0800

    Placed index on parent container

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -6,27 +6,26 @@ const Form = ({ heading, inputFields }) => {
       <form>
         <h2>{heading}</h2>
         {inputFields.map((field, index) => {
-          return <>
+          return <div key={index}>
             {/* This conditional rendering of the field heading allows you to
               create and separate groups of input fields in a form.
               i.e. text inputs -> radio inputs
               NOTE: Ensure the first input in the set has the 'heading' key */}
             {field.heading ? <h3>{field.heading}</h3> : null}
             <input
-              index={index}
               name={field.name}
               placeholder={field.placeholder || null}
               required={field.required || null}
               type={field.type || 'text'}
-              value={field.value || null}
+              value={field.value || ''}
           />
             {/* Depending on the type of input, different attributes will be 
             linked to the 'for' attribute on the label element */}
-            {field.label && field.placeholder ? <label for={field.placeholder}>{field.label}</label> : null}
-            {field.label && field.value ? <label for={field.value}>{field.label}</label> : null}
-          </>
+            {field.label && field.placeholder ? <label htmlFor={field.placeholder}>{field.label}</label> : null}
+            {field.label && field.value ? <label htmlFor={field.value}>{field.label}</label> : null}
+          </div>
         })}
-        <input type='submit' value="Submit" />
+        <input type='submit' value='Submit' />
       </form>
     </>
   )
