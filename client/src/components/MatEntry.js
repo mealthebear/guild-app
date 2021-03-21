ef8cd767a007ff046eb405bbdd9a36661e3b8829
mealthebear <mealthebear@gmail.com>
2021-03-21 05:11:18 -0700

    Developed conditional rendering for update inputs

--- client/src/components/MatEntry.js
+++ client/src/components/MatEntry.js
@@ -1,13 +1,30 @@
 import React, { useState } from 'react';
 
 const MatEntry = ({ mat, quantity }) => {
+  const [matName, setMat] = useState('');
+  const [matQuantity, setQuantity] = useState(0);
   const [showNameUpdate, setNameBoolean] = useState(false);
   const [showQuantityUpdate, setQuantityBoolean] = useState(false);
 
   return (
     <div>
-      <h2>{mat}</h2><button type="button">Update Name</button>
-      <p>Quantity in stock: {quantity}</p><button type="button">Update Quantity</button>
+      <span>{mat}</span> 
+      {showNameUpdate ? <>
+      <input name="name" placeholder="Item Name" />
+      <button onClick={() => setNameBoolean(!showNameUpdate)} type="button">Confirm</button>
+      <button onClick={() => setNameBoolean(!showNameUpdate)} type="button">X</button>
+      </> : 
+      <button onClick={setNameBoolean} type="button">Update Name</button>
+      }
+
+      <span>Quantity in stock: {quantity}</span>
+      {showQuantityUpdate ? <>
+      <input name="quantity" placeholder="Quantity" />
+      <button onClick={() => setQuantityBoolean(!showQuantityUpdate)} type="button">Confirm</button>
+      <button onClick={() => setQuantityBoolean(!showQuantityUpdate)} type="button">X</button>
+      </> : 
+      <button onClick={setQuantityBoolean} type="button">Update Quantity</button>
+      }
     </div>
   )
 }
