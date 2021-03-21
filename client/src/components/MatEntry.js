5ce9656e86e6bf545452979d1b2a61187bdfde00
mealthebear <mealthebear@gmail.com>
2021-03-21 04:50:05 -0700

    Added buttons to update mat name and quantity

--- client/src/components/MatEntry.js
+++ client/src/components/MatEntry.js
@@ -3,8 +3,8 @@ import React, { useState } from 'react';
 const MatEntry = ({ mat, quantity }) => {
   return (
     <div>
-      <h2>{mat}</h2>
-      <p>Quantity in stock: {quantity}</p>
+      <h2>{mat}</h2><button type="button">Update Name</button>
+      <p>Quantity in stock: {quantity}</p><button type="button">Update Quantity</button>
     </div>
   )
 }
