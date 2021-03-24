f57e94e837f4ec7a2438c822f968e25b0203f7df
mealthebear <mealthebear@gmail.com>
2021-03-24 04:18:11 -0700

    Incorporated built in parsers in Express server

--- server/index.js
+++ server/index.js
@@ -6,8 +6,8 @@ const router = require('./router.js');
 const app = express();
 const port = 3000;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
 app.use('/api', router);
