8026281e35e77d7a5b4b7e9f9ca86d8381c91631
mealthebear <mealthebear@gmail.com>
2021-03-24 05:13:25 -0700

    Organized mat entries to be stacked vertically

--- client/src/components/MatList.js
+++ client/src/components/MatList.js
@@ -3,11 +3,11 @@ import React, { useState } from 'react';
 
 const MatList = ({ mats }) => {
   return (
-    <>
+    <div className="mat-list">
       {mats.map((mat, index) => {
         return <MatEntry key={index} mat={mat.name} quantity={mat.quantity} />
       })}
-    </>
+    </div>
   )
 }
 
