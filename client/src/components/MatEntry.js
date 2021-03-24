6ab219951232a562c23232a073f21c85c2f2a318
mealthebear <mealthebear@gmail.com>
2021-03-24 06:31:47 -0700

    Added styles for update button

--- client/src/components/MatEntry.js
+++ client/src/components/MatEntry.js
@@ -12,21 +12,21 @@ const MatEntry = ({ mat, quantity }) => {
         <span className="mat-name">{mat}</span>
         {showNameUpdate ? <>
         <input className="form-input mat-entry-input" name="name" placeholder="Item Name" />
-        <button className="mat-entry-button update-button" onClick={() => setNameBoolean(!showNameUpdate)} type="button">Confirm</button>
+        <button className="mat-entry-button confirm-button" onClick={() => setNameBoolean(!showNameUpdate)} type="button">Confirm</button>
         <button className="mat-entry-button cancel-button" onClick={() => setNameBoolean(!showNameUpdate)} type="button">X</button>
         </> :
-        <button onClick={setNameBoolean} type="button">Update Name</button>
+        <button className="mat-entry-button update-button" onClick={setNameBoolean} type="button">Update Name</button>
         }
       </div>
       
-      <div className="mat-section-wrapper">
+      <div className="mat-section-wrapper mat-quantity-wrapper">
         <span className="mat-quantity">Quantity in stock: {quantity}</span>
         {showQuantityUpdate ? <>
-        <input name="quantity" placeholder="Quantity" />
-        <button className="mat-entry-button update-button" onClick={() => setQuantityBoolean(!showQuantityUpdate)} type="button">Confirm</button>
+        <input className="form-input mat-entry-input" name="quantity" placeholder="Quantity" />
+        <button className="mat-entry-button confirm-button" onClick={() => setQuantityBoolean(!showQuantityUpdate)} type="button">Confirm</button>
         <button className="mat-entry-button cancel-button" onClick={() => setQuantityBoolean(!showQuantityUpdate)} type="button">X</button>
         </> :
-        <button onClick={setQuantityBoolean} type="button">Update Quantity</button>
+        <button className="mat-entry-button update-button" onClick={setQuantityBoolean} type="button">Update Quantity</button>
         }
       </div>
     </div>
