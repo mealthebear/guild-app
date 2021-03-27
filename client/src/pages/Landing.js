d255c6424395721cef5892612d419b8b5af655c4
mealthebear <mealthebear@gmail.com>
2021-03-27 04:30:41 -0700

    Added a message button on the landing page

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -2,19 +2,22 @@ import React, { useEffect, useState } from 'react';
 
 const Landing = () => {
   return (
-    <div>
+    <>
       <div className="heading-landing-container">
-      <img className="header-image" src="./images/wow-cat.jpg" />
-      <div className="heading-landing-text-box">
-        <h2 className="heading heading-landing">Hello, we are Défiance</h2>
-        <p className="heading-landing-text">
-          We're a nifty little guild on Alexstrasza / Terokkar. 
-          Interested in raiding with us?
-          Click the button on the right!
-        </p>
+        <img className="header-image" src="./images/wow-cat.jpg" />
+        <div className="heading-landing-text-box">
+          <h2 className="heading heading-landing">Hello, we are Défiance</h2>
+          <div>
+            <p className="heading-landing-text">
+              We're a nifty little guild on Alexstrasza / Terokkar. 
+              Interested in raiding with us?
+              Click the button on the right!
+            </p>
+            <button className="heading-landing-button" type="button">Message Us!</button>
+          </div>
+        </div>
       </div>
-    </div>
-    </div>
+    </>
   )
 }
 
