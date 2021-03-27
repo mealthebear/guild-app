e1b745f37bd025b9d460a7dd7948ea15ae6833e3
mealthebear <mealthebear@gmail.com>
2021-03-27 03:57:51 -0700

    Created landing header with image and text

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -3,7 +3,17 @@ import React, { useEffect, useState } from 'react';
 const Landing = () => {
   return (
     <div>
-      This is the landing page
+      <div className="heading-landing-container">
+      <img className="header-image" src="./images/wow-cat.jpg" />
+      <div className="heading-landing-text-box">
+        <h2 className="heading heading-landing">Hello, we are Défiance</h2>
+        <p className="heading-landing-text">
+          We're a nifty little guild on Alexstrasza / Terokkar. 
+          Interested in raiding with us?
+          Click the button on the right!
+        </p>
+      </div>
+    </div>
     </div>
   )
 }
