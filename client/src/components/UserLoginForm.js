73e9728ecde043c2e8d73a60ff31b9421fd94ebd
mealthebear <mealthebear@gmail.com>
2021-03-29 03:04:26 -0700

    Changed color scheme of login form

--- client/src/components/UserLoginForm.js
+++ client/src/components/UserLoginForm.js
@@ -2,7 +2,10 @@ import React, { useState } from 'react';
 
 const UserLoginForm = (props) => {
   return (
+    <>
+    <img className="blue-gradient" src="./images/blue-gradient.jpg" />
     <form className="login-form">
+      <h2 className="subheading subheading-login">Login To Your Account</h2>
       <div className="input-field-group">
         <label className="form-label">Username</label>
         <input className="form-input form-input-login" placeholder="Enter Username" required />
@@ -13,6 +16,7 @@ const UserLoginForm = (props) => {
       </div>
       <input className="confirm-button confirm-button-login" type="submit" value="Login" />
     </form>
+    </>
   )
 }
 
