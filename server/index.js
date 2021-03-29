61625de18ba00000dbf32d95b6e246019bda8647
mealthebear <mealthebear@gmail.com>
2021-03-29 05:55:52 -0700

    Developed endpoint to create JSON Web Tokens

--- server/index.js
+++ server/index.js
@@ -1,5 +1,8 @@
+require('dotenv').config()
+
 const bodyParser = require('body-parser');
 const express = require('express');
+const jwt = require('jsonwebtoken');
 const path = require('path');
 const router = require('./router.js');
 
@@ -11,6 +14,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
 app.use('/api', router);
+
+app.post('/auth', (req, res) => {
+  const username = req.body.username;
+  const user = { user: username };
+
+  const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+
+  res.cookie('accessToken', accessToken, { httpOnly: true });
+
+  res.json({ accessToken: accessToken });
+})
 
 app.get('/bank', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/dist/index.html'), (err) => {
