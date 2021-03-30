7b41a660772d0578272c54726ecc038601a76ad7
mealthebear <mealthebear@gmail.com>
2021-03-30 07:35:23 -0700

    Styled messages and added media queries

--- client/src/components/LoginMessage.js
+++ client/src/components/LoginMessage.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 
 const LoginMessage = (props) => {
-  const errorMessage = <p>Username and/or password invalid. Please try again.</p>
-  const successMessage = <p>You've successfully logged in! You may access the inventory page.</p>
+  const errorMessage = <p className="login-message error-message">Username and/or password invalid. Please try again.</p>
+  const successMessage = <p className="login-message success-message">You've successfully logged in! You may access the inventory page.</p>
 
   return (
     <>
